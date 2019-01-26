c1dcc5bb5faab1acacf1345b226f52bb4a0f6748
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-26 19:23:28 +0100

    Update the comments to point to the README with BT services & characteristics info

--- index.js
+++ index.js
@@ -4,22 +4,13 @@ window.addEventListener('load', _ => {
   const gestureButton = document.createElement('button');
   gestureButton.textContent = 'Pair';
   gestureButton.addEventListener('click', async _ => {
-    // Onewheels show up under *Onewheel* before pairing and *ow#######* once paired
+    // Note that Onewheels show up under *Onewheel* before pairing and *ow#######* once paired
     // TODO: Extend the filters to also see an unpaired Onewheel (need to unpair the existing one for that)
     const bluetoothDevice = await navigator.bluetooth.requestDevice({
       filters: [ { namePrefix: 'ow' } ],
       optionalServices: [
-        // Note that some Bluetooth services are standardized: https://www.bluetooth.com/specifications/gatt/services
-        'battery_service',
-        
-        /* Note that Onewheel proprietary services & characteristics are taken from the Internet, probably reversing-engineered
-           - https://github.com/drewbaumann/onewheel-web/blob/master/script/master.js
-           - https://community.onewheel.com/topic/2002/ble-commands-for-battery-status-etc
-           - https://pastebin.com/Mu35jQGz
-        */
-        // TODO: Plan on disassembling the Android application to find the hardcoded service UUIDs
-        
-        // Note that this should be the Onewheel BTLE service
+        // See the README for info on how to discover the available services & what characteristics are known
+        // Note that this is the right BTLE service and the OW provides some others
         'e659f300-ea98-11e3-ac10-0800200c9a66',
       ]
     });
