9c553a0d919ff4af7bfbe0c93f5e821a0e2e5e7b
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-26 18:41:27 +0100

    Connect and log the primary BTLE service

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 window.addEventListener('load', _ => {
-  console.log('Ready…');
+  // Note that Web Blueooth must be initiated from a user gesture for security reasons
+  const gestureButton = document.createElement('button');
+  gestureButton.textContent = 'Pair';
+  gestureButton.addEventListener('click', async _ => {
+    // Onewheels show up under *Onewheel* before pairing and *ow#######* once paired
+    // TODO: Extend the filters to also see an unpaired Onewheel (need to unpair the existing one for that)
+    const bluetoothDevice = await navigator.bluetooth.requestDevice({
+      filters: [ { namePrefix: 'ow' } ],
+      optionalServices: [
+        // Note that some Bluetooth services are standardized: https://www.bluetooth.com/specifications/gatt/services
+        'battery_service',
+        
+        /* Note that Onewheel proprietary services & characteristics are taken from the Internet, probably reversing-engineered
+           - https://github.com/drewbaumann/onewheel-web/blob/master/script/master.js
+           - https://community.onewheel.com/topic/2002/ble-commands-for-battery-status-etc
+           - https://pastebin.com/Mu35jQGz
+        */
+        // TODO: Plan on disassembling the Android application to find the hardcoded service UUIDs
+        
+        // Note that this should be the Onewheel BTLE service
+        'e659f300-ea98-11e3-ac10-0800200c9a66',
+      ]
+    });
+    
+    const gattServer = await bluetoothDevice.gatt.connect();
+    const service = await gattServer.getPrimaryService('e659f300-ea98-11e3-ac10-0800200c9a66');
+    console.log(service);
+  });
 });
